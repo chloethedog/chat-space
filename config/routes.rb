3d03b044d688027d4b1f81a02d9e241e074741af
chloethedog <hitomiandsakura@hotmail.com>
2020-12-26 15:29:55 +0900

    自動更新機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update, :index] 
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json'}
+    end
+  end  
 end
